16e05e7cd8211bfc7e996d923ad7836bd8e4dc5a
jiamingsuper@gmail.com <jiamingsuper@gmail.com>
2024-03-04 09:52:39 +0800

    feat: custom file

--- change_config.sh
+++ change_config.sh
@@ -11,22 +11,28 @@ ip_address=$1
 number=$2
 option=$3
 
+cp ./scripts/index.txt ./scripts/index.ts
+cp sequencer_config.txt sequencer_config.json
+cp deployed_chain_info.txt deployed_chain_info.json
+cp deployment.txt deployment.json
+cp l2_chain_info.txt l2_chain_info.json
+
 sed -i "s/\"url\": \"ws:\/\/geth:8546\"/\"url\": \"ws:\/\/$ip_address:8546\"/" sequencer_config.json
 sed -i "s/ws:\/\/geth:8546/ws:\/\/$ip_address:8546/" ./scripts/index.ts
 
 # 根据第三个参数进行不同的替换操作
 case $option in
     1)
-        sed -i "s/\"0x46225F4cee2b4A1d506C7f894bb3dAeB21BF1596\"/\"ABC\"/" sequencer_config.json
-        sed -i "s/\"0x19ED240ddd4DDEeDdF2B77aA279F258eFC52f9b7\"/\"ABCD\"/" sequencer_config.json
+        sed -i "s/\"0x6A568afe0f82d34759347bb36F14A6bB171d2CBe\"/\"0x46225F4cee2b4A1d506C7f894bb3dAeB21BF1596\"/" sequencer_config.json
+        sed -i "s/\"0xe2148eE53c0755215Df69b2616E552154EdC584f\"/\"0x19ED240ddd4DDEeDdF2B77aA279F258eFC52f9b7\"/" sequencer_config.json
         ;;
     2)
-        sed -i "s/\"0xaDef93B37f314ae8c8d711C76dB2C17587738DB8\"/\"BCD\"/" sequencer_config.json
-        sed -i "s/\"0xcEeD7eFb59a5De0B885Af9b5D416bc5E9725C8e0\"/\"BCEDE\"/" sequencer_config.json
+        sed -i "s/\"0x6A568afe0f82d34759347bb36F14A6bB171d2CBe\"/\"0xaDef93B37f314ae8c8d711C76dB2C17587738DB8\"/" sequencer_config.json
+        sed -i "s/\"0xe2148eE53c0755215Df69b2616E552154EdC584f\"/\"0xcEeD7eFb59a5De0B885Af9b5D416bc5E9725C8e0\"/" sequencer_config.json
         ;;
     3)
-        sed -i "s/\"0xb911217f08f23e49F02151354c91c3f213C040CE\"/\"12345\"/" sequencer_config.json
-        sed -i "s/\"0x0831A8b22377779D19d014810754ceD6fC6b7dA0\"/\"123456\"/" sequencer_config.json
+        sed -i "s/\"0x6A568afe0f82d34759347bb36F14A6bB171d2CBe\"/\"0xb911217f08f23e49F02151354c91c3f213C040CE\"/" sequencer_config.json
+        sed -i "s/\"0xe2148eE53c0755215Df69b2616E552154EdC584f\"/\"0x0831A8b22377779D19d014810754ceD6fC6b7dA0\"/" sequencer_config.json
         ;;
     *)
         echo "Invalid option. Please use 1, 2, or 3 for the third parameter."
